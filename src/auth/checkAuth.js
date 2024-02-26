72d1db9843c1319abeb856e227dfb464f852673d
tien nguyen <tiennguyen@okxe.vn>
2024-02-27 01:32:09 +0700

    Handle middlewere check apiKey and permission

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+const apiKey = async (req, res, next) => {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString();
+        if(!key) return res.status(403).json({
+            message: 'Forbidden Error'
+        })
+
+        // check objKey
+        const objKey = await findById(key)
+        if(!objKey) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            })
+        }
+
+        req.objKey = objKey
+        return next()
+    } catch (error) {
+        
+    }
+}
+
+const permission = (permission) => {
+    return async (req, res, next) => {
+        if(!req.objKey.permissions) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            })
+        }
+
+        const validPermission = req.objKey.permissions.includes(permission)
+        if(!validPermission) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            })
+        }
+        return next()
+    }
+}
+
+module.exports = {
+    apiKey,
+    permission
+}
