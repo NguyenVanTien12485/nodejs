99d7d3d9a3a1ed22bbafdb6992909d86babce107
tien nguyen <kudoshinichik50@gmail.com>
2024-04-02 02:03:27 +0700

    Update code

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,56 +1,47 @@
-'use strict'
-const AccessService = require('../services/access.service')
-const {OK, CREATED, SuccessResponse} = require('../core/success.response')
+"use strict";
+const AccessService = require("../services/access.service");
+const { OK, CREATED, SuccessResponse } = require("../core/success.response");
 class AccessController {
-  handleRefreshToken = async (req, res, next) => {
-    // new SuccessResponse(
-    //   {
-    //     message: 'Get token success',
-    //     metadata: await AccessService.handleRefreshToken(req.body.refreshToken),
-    //   }
-    // ).send(res)
+    handleRefreshToken = async (req, res, next) => {
+        // new SuccessResponse(
+        //   {
+        //     message: 'Get token success',
+        //     metadata: await AccessService.handleRefreshToken(req.body.refreshToken),
+        //   }
+        // ).send(res)
 
-    //version 2: fixed no need, access token
-     new SuccessResponse(
-      {
-        message: 'Get token success',
-        metadata: await AccessService.handleRefreshTokenV2({
-          refreshToken: req.refreshToken,
-          user: req.user,
-          keyStore: req.keyStore
-        }),
-      }
-    ).send(res)
-
-  }
+        //version 2: fixed no need, access token
+        new SuccessResponse({
+            message: "Get token success",
+            metadata: await AccessService.handleRefreshTokenV2({
+                refreshToken: req.refreshToken,
+                user: req.user,
+                keyStore: req.keyStore,
+            }),
+        }).send(res);
+    };
 
     logout = async (req, res, next) => {
-      new SuccessResponse(
-        {
-          message: 'Logout success',
-          metadata: await AccessService.logout(req.keyStore),
-        }
-      ).send(res)
-    }
+        new SuccessResponse({
+            message: "Logout success",
+            metadata: await AccessService.logout(req.keyStore),
+        }).send(res);
+    };
     login = async (req, res, next) => {
-      new SuccessResponse(
-        {
-          metadata: await AccessService.login(req.body),
-        }
-      ).send(res)
-    }
-   signUp = async (req, res, next) => {
-      // return res.status(200).json({
-      //    message: 'success',
-      //    metadata: 
-      // })
-      new CREATED(
-        {
-          message: 'Register OK',
-          metadata: await AccessService.signUp(req.body)
-        }
-      ).send(res)
-   }
+        new SuccessResponse({
+            metadata: await AccessService.login(req.body),
+        }).send(res);
+    };
+    signUp = async (req, res, next) => {
+        // return res.status(200).json({
+        //    message: 'success',
+        //    metadata:
+        // })
+        new CREATED({
+            message: "Register OK",
+            metadata: await AccessService.signUp(req.body),
+        }).send(res);
+    };
 }
 
-module.exports = new AccessController()
+module.exports = new AccessController();
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,21 +1,20 @@
-'use strict'
-const ProductService = require('../services/product.service')
-const ProductServiceV2 = require('../services/product.service.xxx')
-const {OK, CREATED, SuccessResponse} = require('../core/success.response')
+"use strict";
+const ProductService = require("../services/product.service");
+const ProductServiceV2 = require("../services/product.service.xxx");
+const { OK, CREATED, SuccessResponse } = require("../core/success.response");
 class ProductController {
-  handleRefreshToken = async (req, res, next) => {
     createProduct = async (req, res, next) => {
-        new SuccessResponse(
-         {
-           message: 'Create new product success',
-           metadata: await ProductServiceV2.createProduct(req.body.product_type, {
-             ...req.body,
-             product_shop: req.user.userId
-           }),
-         }
-       ).send(res)
-    }
-  }
+        new SuccessResponse({
+            message: "Create new product success",
+            metadata: await ProductServiceV2.createProduct(
+                req.body.product_type,
+                {
+                    ...req.body,
+                    product_shop: req.user.userId,
+                }
+            ),
+        }).send(res);
+    };
 }
 
-module.exports = new AccessController()
+module.exports = new ProductController();
